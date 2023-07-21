ff2af476a3adbdf4baa588cced5c6f507ba6394c
Joonas Pelttari <joonas.pelttari98@gmail.com>
2023-07-21 20:47:01 +0300

    currentplayers table, zebra striping

--- src/components/TableComponent.js
+++ src/components/TableComponent.js
@@ -1,16 +1,37 @@
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
+import TableCell, {tableCellClasses} from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
 
 
-  // return number with thousand separators. 1000000=>1.000.000
-  function numberWithSeparators(number) {
-    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
-  }
+// return number with thousand separators. 1000000=>1.000.000
+function numberWithSeparators(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+}
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
 
 const TableComponent = ({data, pageNumber}) => {
   return (
@@ -24,45 +45,45 @@ const TableComponent = ({data, pageNumber}) => {
         size="small" aria-label="a dense table"
       >
         <TableHead>
-          <TableRow>
-            <TableCell style={{ width: '500px', fontSize: '25px', fontWeight: 'bold' }}
-              >Game</TableCell>
-            <TableCell style={{ fontSize: '22px', fontWeight: 'bold' }}
-              align='right'>Current</TableCell>
-            <TableCell style={{ fontSize: '22px', fontWeight: 'bold' }}
-              align='right'>24h peak</TableCell>
-            <TableCell style={{ fontSize: '22px', fontWeight: 'bold' }}
-              align='right'>24h bottom</TableCell>
-          </TableRow>
+          <StyledTableRow>
+            <StyledTableCell style={{ width: '500px', fontSize: '25px', fontWeight: 'bold' }}
+              >Game</StyledTableCell>
+            <StyledTableCell style={{ fontSize: '22px', fontWeight: 'bold' }}
+              align='right'>Current</StyledTableCell>
+            <StyledTableCell style={{ fontSize: '22px', fontWeight: 'bold' }}
+              align='right'>24h peak</StyledTableCell>
+            <StyledTableCell style={{ fontSize: '22px', fontWeight: 'bold' }}
+              align='right'>24h bottom</StyledTableCell>
+          </StyledTableRow>
         </TableHead>
 
         <TableBody>
           {data[pageNumber] && data[pageNumber].map((item, index) => (
-            <TableRow 
+            <StyledTableRow 
               key={item.gameid}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
-              <TableCell component='th' scope='row'
+              <StyledTableCell component='th' scope='row'
                 style={{ fontSize: '18px'}}>
                 {(((pageNumber-1)*10)+index+1) +'. '+ item.gamename}
-              </TableCell>
+              </StyledTableCell>
 
-              <TableCell style={{ fontSize: '17px'}}
+              <StyledTableCell style={{ fontSize: '17px'}}
                 align='right'>
                   {numberWithSeparators(item?.playercount)}
-              </TableCell>
+              </StyledTableCell>
 
-              <TableCell style={{ fontSize: '17px'}}
+              <StyledTableCell style={{ fontSize: '17px'}}
                 align='right'>
                   {numberWithSeparators(item?.peak)}
-              </TableCell>
+              </StyledTableCell>
 
-              <TableCell style={{ fontSize: '17px'}}
+              <StyledTableCell style={{ fontSize: '17px'}}
                 align='right'>
                   {numberWithSeparators(item?.bottom)}
-              </TableCell>
+              </StyledTableCell>
               
-            </TableRow>
+            </StyledTableRow>
           ))}
         </TableBody>
       </Table>
